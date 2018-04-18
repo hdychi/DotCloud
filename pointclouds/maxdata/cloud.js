27eced2449cf16f3c4178d246785bad575851e00
hdychi <547690378@qq.com>
2018-04-18 15:09:11 +0800

    可视化项目

---
+++ pointclouds/maxdata/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 70000000,
+    "boundingBox": {
+        "lx": 9028.692,
+        "ly": -8214.492,
+        "lz": 20.175,
+        "ux": 12639.080999999998,
+        "uy": -4604.103,
+        "uz": 3630.5640000000003
+    },
+    "tightBoundingBox": {
+        "lx": 9028.692,
+        "ly": -8214.492,
+        "lz": 20.175,
+        "ux": 10126.854,
+        "uy": -4604.103,
+        "uz": 117.643
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 31.26688575744629,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
